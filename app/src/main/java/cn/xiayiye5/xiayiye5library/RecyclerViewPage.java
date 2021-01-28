e133e0da9dc325fedee639679bdfdd39b79ddf76
zhengcong <13343401268@qq.com>
2021-01-28 15:55:39 +0800

    增加API29获取目录失效的方法

--- app/src/main/java/cn/xiayiye5/xiayiye5library/RecyclerViewPage.java
+++ app/src/main/java/cn/xiayiye5/xiayiye5library/RecyclerViewPage.java
@@ -1,6 +1,7 @@
 package cn.xiayiye5.xiayiye5library;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +16,7 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -63,6 +65,56 @@ public class RecyclerViewPage extends AppCompatActivity {
                 super.onScrollStateChanged(recyclerView, newState);
             }
         });
+        File externalFilesDir1 = getExternalFilesDir(Environment.DIRECTORY_ALARMS);
+        File externalFilesDir2 = getExternalFilesDir(Environment.DIRECTORY_AUDIOBOOKS);
+        File externalFilesDir3 = getExternalFilesDir(Environment.DIRECTORY_DCIM);
+        File externalFilesDir4 = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+        File externalFilesDir5 = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS);
+        File externalFilesDir6 = getExternalFilesDir(Environment.DIRECTORY_MOVIES);
+        File externalFilesDir7 = getExternalFilesDir(Environment.DIRECTORY_MUSIC);
+        File externalFilesDir8 = getExternalFilesDir(Environment.DIRECTORY_NOTIFICATIONS);
+        File externalFilesDir9 = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File externalFilesDir10 = getExternalFilesDir(Environment.DIRECTORY_PODCASTS);
+        File externalFilesDir11 = getExternalFilesDir(Environment.DIRECTORY_RINGTONES);
+        File externalFilesDir12 = getExternalFilesDir(Environment.DIRECTORY_SCREENSHOTS);
+        File externalFilesDir13 = getExternalFilesDir(Environment.DIRECTORY_RINGTONES);
+        File externalFilesDir14 = getExternalFilesDir(Environment.MEDIA_BAD_REMOVAL);
+        File externalFilesDir15 = getExternalFilesDir(Environment.MEDIA_CHECKING);
+        File externalFilesDir16 = getExternalFilesDir(Environment.MEDIA_EJECTING);
+        File externalFilesDir17 = getExternalFilesDir(Environment.MEDIA_MOUNTED);
+        File externalFilesDir18 = getExternalFilesDir(Environment.MEDIA_MOUNTED_READ_ONLY);
+        File externalFilesDir19 = getExternalFilesDir(Environment.MEDIA_NOFS);
+        File externalFilesDir20 = getExternalFilesDir(Environment.MEDIA_REMOVED);
+        File externalFilesDir21 = getExternalFilesDir(Environment.MEDIA_SHARED);
+        File externalFilesDir22 = getExternalFilesDir(Environment.MEDIA_UNKNOWN);
+        File externalFilesDir23 = getExternalFilesDir(Environment.MEDIA_UNMOUNTABLE);
+        File externalFilesDir24 = getExternalFilesDir(Environment.MEDIA_UNMOUNTED);
+        Log.e("打印路径", externalFilesDir1.getAbsolutePath()
+                + "\n" + externalFilesDir1.getAbsolutePath()
+                + "\n" + externalFilesDir1.getAbsolutePath()
+                + "\n" + externalFilesDir2.getAbsolutePath()
+                + "\n" + externalFilesDir3.getAbsolutePath()
+                + "\n" + externalFilesDir4.getAbsolutePath()
+                + "\n" + externalFilesDir5.getAbsolutePath()
+                + "\n" + externalFilesDir6.getAbsolutePath()
+                + "\n" + externalFilesDir7.getAbsolutePath()
+                + "\n" + externalFilesDir8.getAbsolutePath()
+                + "\n" + externalFilesDir9.getAbsolutePath()
+                + "\n" + externalFilesDir10.getAbsolutePath()
+                + "\n" + externalFilesDir11.getAbsolutePath()
+                + "\n" + externalFilesDir12.getAbsolutePath()
+                + "\n" + externalFilesDir13.getAbsolutePath()
+                + "\n" + externalFilesDir14.getAbsolutePath()
+                + "\n" + externalFilesDir15.getAbsolutePath()
+                + "\n" + externalFilesDir16.getAbsolutePath()
+                + "\n" + externalFilesDir17.getAbsolutePath()
+                + "\n" + externalFilesDir18.getAbsolutePath()
+                + "\n" + externalFilesDir19.getAbsolutePath()
+                + "\n" + externalFilesDir20.getAbsolutePath()
+                + "\n" + externalFilesDir21.getAbsolutePath()
+                + "\n" + externalFilesDir22.getAbsolutePath()
+                + "\n" + externalFilesDir23.getAbsolutePath()
+                + "\n" + externalFilesDir24.getAbsolutePath());
     }
 
     static class MyAdapter extends RecyclerView.Adapter<MyAdapter.DemoHolder> {
