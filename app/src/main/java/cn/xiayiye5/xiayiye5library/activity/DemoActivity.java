3f3d47719453b0d537e7de450407965173b32183
zhengcong <13343401268@qq.com>
2021-03-02 11:41:15 +0800

    1.通过反射创建对象

--- app/src/main/java/cn/xiayiye5/xiayiye5library/activity/DemoActivity.java
+++ app/src/main/java/cn/xiayiye5/xiayiye5library/activity/DemoActivity.java
@@ -1,15 +1,21 @@
 package cn.xiayiye5.xiayiye5library.activity;
 
+import android.app.Application;
 import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 
 import com.alibaba.android.arouter.launcher.ARouter;
 
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 import cn.xiayiye5.xiayiye5library.R;
 import cn.xiayiye5.xiayiye5library.bean.ObjName;
 import cn.xiayiye5.xiayiye5library.bean.ObjNameSer;
 import cn.xiayiye5.xiayiye5library.comment.RouterValue;
+import cn.xiayiye5.xiayiye5library.utils.XiaYiYe5Utils;
 
 /**
  * @author : xiayiye5
@@ -25,6 +31,29 @@ public class DemoActivity extends BaseActivity {
     @Override
     protected void initId() {
         Log.e("打印初始化DemoActivity", "id成功");
+        //通过反射创建对象
+        try {
+            Class<?> aClass = Class.forName("cn.xiayiye5.xiayiye5library.utils.XiaYiYe5Utils");
+            Constructor<?> declaredConstructor = aClass.getDeclaredConstructor();
+            //暴力反射
+            declaredConstructor.setAccessible(true);
+            Object o = declaredConstructor.newInstance();
+            Log.e("打印对象", o.getClass().getSimpleName());
+            if (o instanceof XiaYiYe5Utils) {
+                Application newApplication = ((XiaYiYe5Utils) o).getNewApplication();
+                Log.e("打印对象", newApplication.getClass().getSimpleName());
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -43,7 +72,7 @@ public class DemoActivity extends BaseActivity {
     }
 
     public void jumpWithObject(View view) {
-        ObjName data = new ObjName("test",20,"北京市东直门外大街230号");
+        ObjName data = new ObjName("test", 20, "北京市东直门外大街230号");
 //        ARouter.getInstance().build(RouterValue.WITH_PARAM_OBJECT).withObject("objName", data).navigation();
     }
 
